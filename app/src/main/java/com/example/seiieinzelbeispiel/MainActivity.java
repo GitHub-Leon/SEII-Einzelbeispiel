1ff88418f2e0a2b3c72673f7e6dc8563caad0c31
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-03-05 14:31:39 +0100

    added 2.2
    added 2.2
    finished project

--- app/src/main/java/com/example/seiieinzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/seiieinzelbeispiel/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.seiieinzelbeispiel;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,9 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.seiieinzelbeispiel.network.NetworkClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
-    EditText matrikelnummerInput;
     Button send_Button;
+    Button berechne_Button;
+    EditText matrikelnummerInput;
     TextView response_server_text;
 
     @Override
@@ -20,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         send_Button = findViewById(R.id.send_button);
+        berechne_Button = findViewById(R.id.berechne_button);
         matrikelnummerInput = findViewById(R.id.matrikelnummer_input);
         response_server_text = findViewById(R.id.response_server);
 
@@ -28,6 +38,29 @@ public class MainActivity extends AppCompatActivity {
             networkClientThread.start();
 
         });
+
+        berechne_Button.setOnClickListener(view -> {
+            ArrayList<Character> result = new ArrayList<>();
+            for (char c : matrikelnummerInput.getText().toString().toCharArray()) {
+                if (!isPrime(c)) {
+                    result.add(c);
+                }
+            }
+
+            Collections.sort(result);
+            String resultString = TextUtils.join("", result); //create one string out of the result array list
+            response_server_text.setText(resultString);
+        });
+    }
+
+    private boolean isPrime(char c) {
+        if (Character.getNumericValue(c) == 1) return false;
+        for (int i = 2; i < Character.getNumericValue(c); i++) {
+            if (Character.getNumericValue(c) % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
